47472e3e5525702e8dc29c2b929cd9850ab1f833
LunasaVT <me@lunasa.dev>
2026-07-26 21:13:14 +0530

    chore: bump max framerate to 120 in GUIs

--- src/main/java/re/lilith/kalia/mixins/render/MixinMinecraftClient.java
+++ src/main/java/re/lilith/kalia/mixins/render/MixinMinecraftClient.java
@@ -6,9 +6,7 @@ import net.minecraft.client.texture.TextureManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import re.lilith.kalia.KaliaHooks;
@@ -20,6 +18,11 @@ public abstract class MixinMinecraftClient {
 
     @Unique
     private boolean kalia$insideLoadingScreen;
+
+    @ModifyConstant(method = "getMaxFramerate", constant = @Constant(intValue = 30))
+    int impl$getMaxFramerate(int constant) {
+        return 120;
+    }
 
     @Inject(method = "loadLogo", at = @At("HEAD"), cancellable = true)
     private void kalia$frameLoadingScreen(TextureManager textureManager, CallbackInfo callback) {
