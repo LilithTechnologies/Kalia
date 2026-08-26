247185fe370980d35f51897ec4a7860ea8dadc66
ohlunasa <me@lunasa.dev>
2026-08-26 21:12:38 +0530

    fix: use correct defaults for fxaa

--- src/main/kotlin/re/lilith/kalia/frame/graph/aa/AaSettings.kt
+++ src/main/kotlin/re/lilith/kalia/frame/graph/aa/AaSettings.kt
@@ -1,8 +1,8 @@
 package re.lilith.kalia.frame.graph.aa
 
 object AaSettings {
-    var fxaaMode = FxaaMode.FAST
-    var upscaleMode = UpscaleMode.BILINEAR
+    var fxaaMode = FxaaMode.OFF
+    var upscaleMode = UpscaleMode.NEAREST
     var upscaleSharpness = 0.6f
     var worldDownscale = 1f
 }
